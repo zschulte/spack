472b4f78e057a5a0afcce26ce8f3d839772f51a2
Gregory Becker <becker33@llnl.gov>
2018-06-25 14:21:10 -0700

    Change from modulecmd interface to module interface
    Update tests
    
    Update Spack core to use new `module` function instead of `get_module_cmd`
    
    Update `flag_handlers` test to properly restore environment which was affecting module tests

--- lib/spack/spack/test/flag_handlers.py
+++ lib/spack/spack/test/flag_handlers.py
@@ -34,7 +34,8 @@ import spack.build_environment
 def temp_env():
     old_env = os.environ.copy()
     yield
-    os.environ = old_env
+    os.environ.clear()
+    os.environ.update(old_env)
 
 
 def add_o3_to_build_system_cflags(pkg, name, flags):
@@ -187,3 +188,4 @@ class TestFlagHandlers(object):
                         '-DCMAKE_CXX_STANDARD_LIBRARIES=-lfoo',
                         '-DCMAKE_Fortran_STANDARD_LIBRARIES=-lfoo'])
         assert set(pkg.cmake_flag_args) == expected
+
